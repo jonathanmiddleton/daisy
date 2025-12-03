e3b00a7c893c5b0361aef4b31af627930c913fe9
jonathanmiddleton <jonathanmiddleton@me.com>
2025-12-03 11:43:47 -0500

    nccl test

--- tools/simple_nccl_test.py
+++ tools/simple_nccl_test.py
@@ -1,12 +1,23 @@
-import os, torch, torch.distributed as dist
+import os
+import torch
+import torch.distributed as dist
 
-dist.init_process_group("nccl")
-rank = dist.get_rank()
-local_rank = int(os.environ['LOCAL_RANK'])
-print("rank", rank)
-print("local_rank", local_rank)
-x = torch.ones(1, device=f"cuda:{local_rank}")
-dist.all_reduce(x)
-print("rank", rank, "x", x.item())
-dist.barrier(device_ids=[f"cuda:{local_rank}"])
-dist.destroy_process_group()
+def main():
+    dist.init_process_group("nccl")
+
+    rank = dist.get_rank()
+    local_rank = int(os.environ["LOCAL_RANK"])
+    assert 0 <= local_rank < torch.cuda.device_count(), "LOCAL_RANK out of range"
+    torch.cuda.set_device(local_rank)
+
+    print("rank: ", rank)
+    print("local_rank: ", local_rank)
+
+    x = torch.ones(1, device=f"cuda:{local_rank}")
+    dist.all_reduce(x)
+    print("rank", rank, "x", x.item())
+    dist.barrier()
+    dist.destroy_process_group()
+
+if __name__ == "__main__":
+    main()
