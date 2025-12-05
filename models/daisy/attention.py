d05f293e4912a6d22e946f82c30685efcbf05e23
jonathanmiddleton <jonathanmiddleton@me.com>
2025-12-05 09:21:15 -0500

    logging

--- models/daisy/attention.py
+++ models/daisy/attention.py
@@ -96,7 +96,8 @@ class CausalSelfAttention(nn.Module):
         self.g_ve = nn.Parameter(torch.tensor(0.0)) if self.receives_ve else None # init 50/50 gate
 
     def maybeResizeRotary(self, newSize: int):
-        if logger.isDebugEnabled(): logger.debug(f"Rotary capacity: {self.rotary._max_seq_len} <-> input_seq_len {newSize}")
+        if logger.isDebugEnabled(): logger.debug(f"maybeResizeRotary newSize={newSize}")
+        if logger.isDebugEnabled(): logger.debug(f"Rotary capacity= {self.rotary._max_seq_len}")
         if newSize <= self.rotary._max_seq_len: return
         if logger.isDebugEnabled(): logger.debug(f"Resizing Rotary to {newSize} positions.")
         from math import pow
